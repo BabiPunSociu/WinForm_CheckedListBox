91f9ced8ef8e963a633f34097bc514a1720e536c
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-06 23:12:53 +0700

    DungNguyen

---
+++ Bai8_Trang68/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Bai8_Trang68
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnHoanThanh = new System.Windows.Forms.Button();
+            this.clbMenu = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // btnHoanThanh
+            // 
+            this.btnHoanThanh.BackColor = System.Drawing.Color.Turquoise;
+            this.btnHoanThanh.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnHoanThanh.Location = new System.Drawing.Point(0, 392);
+            this.btnHoanThanh.Name = "btnHoanThanh";
+            this.btnHoanThanh.Size = new System.Drawing.Size(800, 58);
+            this.btnHoanThanh.TabIndex = 0;
+            this.btnHoanThanh.Text = "&Hoàn Thành";
+            this.btnHoanThanh.UseVisualStyleBackColor = false;
+            this.btnHoanThanh.Click += new System.EventHandler(this.btnHoanThanh_Click);
+            // 
+            // clbMenu
+            // 
+            this.clbMenu.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.clbMenu.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clbMenu.FormattingEnabled = true;
+            this.clbMenu.Location = new System.Drawing.Point(0, 0);
+            this.clbMenu.Name = "clbMenu";
+            this.clbMenu.Size = new System.Drawing.Size(800, 392);
+            this.clbMenu.TabIndex = 1;
+            this.clbMenu.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.clbMenu_ItemCheck);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGreen;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clbMenu);
+            this.Controls.Add(this.btnHoanThanh);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CheckedListBox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHoanThanh;
+        private System.Windows.Forms.CheckedListBox clbMenu;
+    }
+}
+
---
+++ Bai8_Trang68/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai8_Trang68
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Cho phép hiển thị trên nhiều cột
+            clbMenu.MultiColumn = true;
+            //Cho phép hiển thị trên n cột
+            clbMenu.ColumnWidth = clbMenu.Width / 3;
+            //Thêm các phần tử vào CheckedListBox
+            clbMenu.Items.Add("Ba chỉ cuốn nắm");
+            clbMenu.Items.Add("Ba chỉ luộc chấm mắm tép");
+            clbMenu.Items.Add("Thịt gà rang muối");
+            clbMenu.Items.Add("Thịt gà nướng");
+            clbMenu.Items.Add("Bê nướng");
+            clbMenu.Items.Add("Chả cá lã vọng");
+            clbMenu.Items.Add("Bún ốc");
+            clbMenu.Items.Add("Cháo chim bồ câu");
+            clbMenu.Items.Add("Cháo sườn");
+            clbMenu.Items.Add("Đậu rán");
+            clbMenu.Items.Add("Rau muống xào tỏi");
+            clbMenu.Items.Add("Rau bí ngô luộc");
+
+            clbMenu.Items.Add("Ba chỉ cuốn nắm");
+            clbMenu.Items.Add("Ba chỉ luộc chấm mắm tép");
+            clbMenu.Items.Add("Thịt gà rang muối");
+            clbMenu.Items.Add("Thịt gà nướng");
+            clbMenu.Items.Add("Bê nướng");
+            clbMenu.Items.Add("Chả cá lã vọng");
+            clbMenu.Items.Add("Bún ốc");
+            clbMenu.Items.Add("Cháo chim bồ câu");
+            clbMenu.Items.Add("Cháo sườn");
+            clbMenu.Items.Add("Đậu rán");
+            clbMenu.Items.Add("Rau muống xào tỏi");
+            clbMenu.Items.Add("Rau bí ngô luộc");
+
+            clbMenu.Items.Add("Ba chỉ cuốn nắm");
+            clbMenu.Items.Add("Ba chỉ luộc chấm mắm tép");
+            clbMenu.Items.Add("Thịt gà rang muối");
+            clbMenu.Items.Add("Thịt gà nướng");
+            clbMenu.Items.Add("Bê nướng");
+            clbMenu.Items.Add("Chả cá lã vọng");
+            clbMenu.Items.Add("Bún ốc");
+            clbMenu.Items.Add("Cháo chim bồ câu");
+            clbMenu.Items.Add("Cháo sườn");
+            clbMenu.Items.Add("Đậu rán");
+            clbMenu.Items.Add("Rau muống xào tỏi");
+            clbMenu.Items.Add("Rau bí ngô luộc");
+
+            clbMenu.Items.Add("Ba chỉ cuốn nắm");
+            clbMenu.Items.Add("Ba chỉ luộc chấm mắm tép");
+            clbMenu.Items.Add("Thịt gà rang muối");
+            clbMenu.Items.Add("Thịt gà nướng");
+            clbMenu.Items.Add("Bê nướng");
+            clbMenu.Items.Add("Chả cá lã vọng");
+            clbMenu.Items.Add("Bún ốc");
+            clbMenu.Items.Add("Cháo chim bồ câu");
+            clbMenu.Items.Add("Cháo sườn");
+            clbMenu.Items.Add("Đậu rán");
+            clbMenu.Items.Add("Rau muống xào tỏi");
+            clbMenu.Items.Add("Rau bí ngô luộc");
+        }
+
+        private void btnHoanThanh_Click(object sender, EventArgs e)
+        {
+            string str = "";
+            foreach (string item in clbMenu.CheckedItems)
+                str = str + item + ", ";
+            //Xóa dấu phẩy và dấu cách thừa cuối chuỗi
+            str = str.Remove(str.Length - 2, 2);
+            MessageBox.Show("Bạn đã chọn các món sau: " + str);
+        }
+
+        private void clbMenu_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (e.NewValue == CheckState.Checked)
+                MessageBox.Show("Bạn vừa chọn thêm món: " + clbMenu.SelectedItem,
+                "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
